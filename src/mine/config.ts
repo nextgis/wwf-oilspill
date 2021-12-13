96d9eaa3f389342268d4aeded451060cb323e65b
Artemiy <rendrom@gmail.com>
2021-12-13 15:38:49 +0200

    feat: set osm for mine app

--- src/mine/config.ts
+++ src/mine/config.ts
@@ -4,7 +4,7 @@ import { MapOptions, paint, styles } from '../config';
 
 export const mapOptions: MapOptions = {
   ngwMapOptions: {
-    qmsId: 2550,
+    osm: true,
     baseUrl: 'https://barents-kara-xprojects.nextgis.com',
     center: [60, 65],
     zoom: 4,
@@ -30,11 +30,7 @@ export const mapOptions: MapOptions = {
       },
     ],
   },
-  // basemap: {
-  //   url: 'http://tiles.maps.sputnik.ru/tiles/kmt2/{z}/{x}/{y}.png',
-  //   attribution:
-  //     "&copy; <a href=http://osm.org/copyright'>OpenStreetMap</a> contributors;Спутник &copy; Ростелеком",
-  // },
+
   minOffsetRight: 460,
 
   styles: styles,
